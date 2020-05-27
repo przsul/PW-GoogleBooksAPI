0384a007f29b7001c43b484401ee850abc8724ec
Kunislaw <weyna.karol@gmail.com>
2020-05-27 20:28:06 +0200

    add pagination

--- src/main/java/GoogleBooksAPI/Controller/MainWindowController.java
+++ src/main/java/GoogleBooksAPI/Controller/MainWindowController.java
@@ -32,15 +32,25 @@ public class MainWindowController {
     @FXML
     private Button searchQueryButton;
 
+    @FXML
+    private Button previousPageButton;
+
+    @FXML
+    private Button nextPageButton;
+
+    private int actualPage = 1;
+    private int maxResults = 40;
+    private String lastQuery = "";
+
     public MainWindowController() {}
 
     @FXML
     void initialize() throws ClientProtocolException, IOException {
 
-        Observable<ActionEvent> bttnEvents =
+        Observable<ActionEvent> btnSearchQueryEvent =
                 JavaFxObservable.eventsOf(searchQueryButton, ActionEvent.ACTION);
 
-        bttnEvents.subscribe(v -> {
+        btnSearchQueryEvent.subscribe(v -> {
             if(!queryTextField.getText().equals("")){
                 System.out.println("MAM COS WPISANE");
                 System.out.println(queryTextField.getText());
@@ -50,6 +60,29 @@ public class MainWindowController {
             }
         });
 
+        Observable<ActionEvent> btnNextPageEvent =
+                JavaFxObservable.eventsOf(nextPageButton, ActionEvent.ACTION);
+
+        btnNextPageEvent.subscribe(v -> {
+            if(!lastQuery.equals("")){
+                actualPage++;
+                sendQuery(lastQuery);
+            } else {
+
+            }
+        });
+
+        Observable<ActionEvent> btnPreviousPageEvent =
+                JavaFxObservable.eventsOf(previousPageButton, ActionEvent.ACTION);
+
+        btnPreviousPageEvent.subscribe(v -> {
+            if(!lastQuery.equals("")){
+                actualPage--;
+                sendQuery(lastQuery);
+            } else {
+
+            }
+        });
 
 
     }
@@ -57,14 +90,19 @@ public class MainWindowController {
     void sendQuery(String query) throws ClientProtocolException, IOException{
         booksObservableList = FXCollections.observableArrayList();
         Gson gson = new Gson();
-        String data = Request.Get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyANmYiVWSx0mZK2dXyodl-7M2dpV8yTkuY")
+        String data = Request.Get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyANmYiVWSx0mZK2dXyodl-7M2dpV8yTkuY" + "&startIndex=" + returnStartIndexFromPage(actualPage) + "&maxResults=" + new Integer(maxResults).toString())
                 .connectTimeout(1000)
                 .socketTimeout(1000)
                 .execute().returnContent().asString();
+        lastQuery = query;
         ContainerGoogleBook containerGoogleBook = gson.fromJson(data, ContainerGoogleBook.class);
         Item[] books = containerGoogleBook.getItems();
         booksObservableList.addAll(books);
         booksListView.setItems(booksObservableList);
         booksListView.setCellFactory(studentListView -> new ListCellCustom());
+    }
+
+    String returnStartIndexFromPage(int page){
+        return new Integer(((page-1) * maxResults)).toString();
     }
 }

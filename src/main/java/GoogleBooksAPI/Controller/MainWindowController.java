d02dd70a2c81855a861c6fada80d5621a6522738
Kunislaw <weyna.karol@gmail.com>
2020-05-30 12:41:43 +0200

    make full async/RX sendQuery method

--- src/main/java/GoogleBooksAPI/Controller/MainWindowController.java
+++ src/main/java/GoogleBooksAPI/Controller/MainWindowController.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import com.google.gson.Gson;
 
 import io.reactivex.Observable;
+import io.reactivex.internal.operators.observable.ObservableFromArray;
+import io.reactivex.internal.operators.observable.ObservableFromCallable;
 import io.reactivex.rxjavafx.observables.JavaFxObservable;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
@@ -38,15 +41,18 @@ public class MainWindowController {
     @FXML
     private Button nextPageButton;
 
+    @FXML
+    private Label actualPageLabel;
+
     private int actualPage = 1;
     private int maxResults = 40;
+    private int maxPages = 0;
     private String lastQuery = "";
 
     public MainWindowController() {}
 
     @FXML
     void initialize() throws ClientProtocolException, IOException {
-
         Observable<ActionEvent> btnSearchQueryEvent =
                 JavaFxObservable.eventsOf(searchQueryButton, ActionEvent.ACTION);
 
@@ -67,6 +73,11 @@ public class MainWindowController {
             if(!lastQuery.equals("")){
                 actualPage++;
                 sendQuery(lastQuery);
+                actualPageLabel.setText("Aktualna strona: " + actualPage + "/" + maxPages);
+                if(actualPage == maxPages){
+                    nextPageButton.setDisable(true);
+                }
+                previousPageButton.setDisable(false);
             } else {
 
             }
@@ -79,6 +90,11 @@ public class MainWindowController {
             if(!lastQuery.equals("")){
                 actualPage--;
                 sendQuery(lastQuery);
+                actualPageLabel.setText("Aktualna strona: " + actualPage + "/" + maxPages);
+                if(actualPage == 1){
+                    previousPageButton.setDisable(true);
+                }
+                nextPageButton.setDisable(false);
             } else {
 
             }
@@ -87,19 +103,28 @@ public class MainWindowController {
 
     }
 
-    void sendQuery(String query) throws ClientProtocolException, IOException{
-        booksObservableList = FXCollections.observableArrayList();
-        Gson gson = new Gson();
-        String data = Request.Get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyANmYiVWSx0mZK2dXyodl-7M2dpV8yTkuY" + "&startIndex=" + returnStartIndexFromPage(actualPage) + "&maxResults=" + new Integer(maxResults).toString())
+    void sendQuery(String query) {
+        new ObservableFromCallable<>(() -> Request.Get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyANmYiVWSx0mZK2dXyodl-7M2dpV8yTkuY" + "&startIndex=" + returnStartIndexFromPage(actualPage) + "&maxResults=" + new Integer(maxResults).toString())
                 .connectTimeout(1000)
                 .socketTimeout(1000)
-                .execute().returnContent().asString();
-        lastQuery = query;
-        ContainerGoogleBook containerGoogleBook = gson.fromJson(data, ContainerGoogleBook.class);
-        Item[] books = containerGoogleBook.getItems();
-        booksObservableList.addAll(books);
-        booksListView.setItems(booksObservableList);
-        booksListView.setCellFactory(studentListView -> new ListCellCustom());
+                .execute().returnContent().asString()).
+                subscribe((data) -> {
+                    Gson gson = new Gson();
+                    lastQuery = query;
+                    booksObservableList = FXCollections.observableArrayList();
+                    ContainerGoogleBook containerGoogleBook = gson.fromJson(data, ContainerGoogleBook.class);
+                    Item[] books = containerGoogleBook.getItems();
+                    maxPages = (int) Math.ceil(containerGoogleBook.getTotalItems() / (float) maxResults);
+                    actualPageLabel.setText("Aktualna strona: " + actualPage + "/" + maxPages);
+                    new ObservableFromArray<>(books).subscribe((book) -> {
+                        booksObservableList.add(book);
+                    }, throwable -> {
+                    }, () -> {
+                        booksListView.setItems(booksObservableList);
+                        booksListView.setCellFactory(studentListView -> new ListCellCustom());
+                        System.out.println("AABBCCDDD");
+                    });
+                });
     }
 
     String returnStartIndexFromPage(int page){
